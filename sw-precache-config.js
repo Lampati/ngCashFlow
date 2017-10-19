3063ec006611ed0010981446aa1f8753951f366c
flanzani <flanzani@mobilecomputing.com.ar>
2017-10-19 20:26:43 -0300

    otra prueba de cache

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -9,8 +9,12 @@ module.exports = {
     root: 'dist',
     stripPrefix: 'dist/',
     navigateFallback: '/index.html',
+    dontCacheBustUrlsMatching:'/^(?=.*\.\w{1,7}$)/',
 
-   
+    runtimeCaching: [{
+      urlPattern: /cashflow-fl\.firebaseapp\.com/,
+      handler: 'networkFirst'
+    }]
   };
 
 
